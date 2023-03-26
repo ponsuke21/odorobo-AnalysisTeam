d26331255246bf112722440173d05bd67f6e802a
pon_suke <124992593+ponsuke21@users.noreply.github.com>
2023-03-26 19:45:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,28 +3,22 @@ window.onload = function() {
     modal_overlay_element.style.display = "block";
 }
 
-var sha256 = document.getElementById("sha256")
-var xobj = new XMLHttpRequest();
-xobj.open("get","password.txt");
-xobj.onreadystatechange = function() {
-    if (xobj.readyState == 4 && xobj.status == 200) {
-        sha256.textContent = xobj.responseText;
+function login() {
+    var xobj = new XMLHttpRequest();
+    xobj.open("get","password.txt");
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+            console.log(xobj.responseText);
+            var password = document.getElementById('password').value;
+            var hash = hash(password);
+            console.log(hash);
+        }
     }
+    xobj.send();
 }
-xobj.send();
-
 
 function hash(text) {
-    const sha = new jsSHA('SHA-256', 'TEXT');
+    var sha = new jsSHA('SHA-256', 'TEXT');
     sha.update(text);
     return sha.getHash('HEX');
-  }
-
-var text = document.getElementById('password');
-text.oninput = inputChange;
-form.addEventListener('submit', function (event) {
-    const password = document.getElementById('password').value;
-    const hash = hash(password);
-    console.log(hash);
-    event.preventDefault();
-  });
+}
