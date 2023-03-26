ab9386c874b6ac4b5160e7ac95527f35c98399ad
pon_suke <124992593+ponsuke21@users.noreply.github.com>
2023-03-26 20:13:23 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-window.onload = function() {
-    var modal_overlay_element = document.getElementById("modal-overlay");
-    modal_overlay_element.style.display = "block";
-}
 
 function login() {
     var xobj = new XMLHttpRequest();
     xobj.open("get","password.txt");
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == 200) {
-            console.log(xobj.responseText);
             var password = document.getElementById('password').value;
-            var hash = hash(password);
-            console.log(hash);
+            document.getElementById("password").value = '';
+            var hash = hashing(password);
+            if (hash == xobj.responseText) {
+                var modal_overlay_element = document.getElementById("modal-overlay");
+                modal_overlay_element.style.display = "none";
+                var form_element = document.getElementById("form");
+                form_element.style.display = "none";
+            }
         }
     }
     xobj.send();
 }
 
-function hash(text) {
+function hashing(text) {
     var sha = new jsSHA('SHA-256', 'TEXT');
     sha.update(text);
     return sha.getHash('HEX');
